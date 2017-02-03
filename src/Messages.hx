24adb382736f255861fa7bd8a8d6d264acd383ce
Dan Korostelev <nadako@gmail.com>
2017-02-03 14:47:12 +0300

    port restart.gui_script

--- src/Messages.hx
+++ src/Messages.hx
@@ -3,6 +3,8 @@ import defold.types.Message;
 class Messages {
     public static var StartGame(default,never) = new Message<Void>("start_game");
     public static var NextLevel(default,never) = new Message<Void>("next_level");
+    public static var RestartLevel(default,never) = new Message<Void>("restart_level");
+    public static var ToMainMenu(default,never) = new Message<Void>("to_main_menu");
     public static var Hide(default,never) = new Message<Void>("hide");
     public static var Show(default,never) = new Message<Void>("show");
 }
---
+++ src/Restart.hx
@@ -0,0 +1,42 @@
+import Defold.hash;
+using defold.Gui; // also apply gui methods as static extension to GuiNode
+import defold.Go.GoMessages;
+import defold.Msg;
+import defold.types.Hash;
+import defold.types.Message;
+import defold.support.ScriptOnInputAction;
+
+class Restart extends defold.support.GuiScript<{}> {
+    override function on_message<T>(_, message_id:Message<T>, message:T, _) {
+        switch (message_id) {
+            case Messages.Hide:
+                Msg.post("#", GoMessages.disable);
+                Msg.post(".", GoMessages.release_input_focus);
+            case Messages.Show:
+                Msg.post("#", GoMessages.enable);
+                Msg.post(".", GoMessages.acquire_input_focus);
+        }
+    }
+
+    override function on_input(_, action_id:Hash, action:ScriptOnInputAction) {
+        if (action_id == hash("touch") && action.pressed) {
+            var yes = Gui.get_node("yes");
+            var no = Gui.get_node("no");
+            var quit = Gui.get_node("quit");
+
+            if (no.pick_node(action.x, action.y)) {
+                Msg.post("#", Messages.Hide);
+                Msg.post("/board#gui", Messages.Show);
+            } else if (yes.pick_node(action.x, action.y)) {
+                Msg.post("board:/board#script", Messages.RestartLevel);
+                Msg.post("/board#gui", Messages.Show);
+                Msg.post("#", Messages.Hide);
+            } else if (quit.pick_node(action.x, action.y)) {
+                Msg.post("main:/main#script", Messages.ToMainMenu);
+                Msg.post("#", Messages.Hide);
+            }
+        }
+        // Consume all input until we're gone.
+        return true;
+    }
+}
